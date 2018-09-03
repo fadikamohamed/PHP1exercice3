183a65d73a7ae33ba3cecce2e28573ff95b9b6d2
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 14:38:16 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  $km = 1;
+  echo $km . ' ';
+  $km = 3;
+  echo $km . ' ';
+  $km = 125;
+  echo $km;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 3</title>
+  </head>
+  <body>
+
+  </body>
+</html>
